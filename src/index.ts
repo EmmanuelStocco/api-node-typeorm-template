6f7e5a745e9be2a9e0bc26884fab38b0f78029be
Emmanuel Stocco <56724388+EmmanuelStocco@users.noreply.github.com>
2022-11-19 14:56:53 -0300

    feat: cors add

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,19 @@ import express from 'express'
 import { AppDataSource } from './data-source'
 import { errorMiddleware } from './middlewares/error'
 import routes from './routes'
+import cors from 'cors';
 
 AppDataSource.initialize().then(() => {
 	const app = express()
 
 	app.use(express.json())
+	
+	app.use(cors({
+		methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+		origin: '*',		
+		credentials: true,
+		allowedHeaders: '*'
+	}))
 
 	app.use(routes)
 
